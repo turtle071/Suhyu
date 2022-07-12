0b03cce4c2b60ac335382dc590db075393403849
Turtle <emanuelsadhot@gmail.com>
2022-07-12 00:12:33 -0300

    feat: add welcome message event

--- src/events/guildMember/autorole.js
+++ src/events/guildMember/autorole.js
@@ -1,4 +1,5 @@
 const Event = require('../../structures/Event');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Event {
     constructor(client) {
@@ -10,5 +11,14 @@ module.exports = class extends Event {
     run = async(member) => {
         const cargo = member.guild.roles.cache.get('906557014212235285')
         member.roles.add(cargo);
+        const avatarUrl = member.displayAvatarURL({ dynamic: true, size: 2048 })
+
+        const welcomeEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setImage(avatarUrl)
+        .setDescription(`Bem-vindo✨ ${member} ao servidor ${member.guild.name}`)
+
+        member.guild.channels.cache.get('906568789347418152').send({content: `${member}`, embeds: [welcomeEmbed]})
+
     }
 }
