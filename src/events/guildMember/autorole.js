c56fb6287288b74860e45140853df9c8f82b8160
Turtle <emanuelsadhot@gmail.com>
2022-07-16 23:51:46 -0300

    refactor: improved code of message embed

--- src/events/guildMember/autorole.js
+++ src/events/guildMember/autorole.js
@@ -11,12 +11,13 @@ module.exports = class extends Event {
     run = async(member) => {
         const cargo = member.guild.roles.cache.get('906557014212235285')
         member.roles.add(cargo);
-        const avatarUrl = member.displayAvatarURL({ dynamic: true, size: 2048 })
+        const avatarUrl = member.displayAvatarURL({ dynamic: true, size: 1024 })
 
         const welcomeEmbed = new MessageEmbed()
-        .setColor('GREEN')
-        .setImage(avatarUrl)
-        .setDescription(`✨***Bem-vindo***✨ ${member} ***ao servidor*** ${member.guild.name} 🐢`)
+            .setTitle('Bem-vindo ao servidor!')
+            .setColor('GREEN')
+            .setImage(avatarUrl)
+            .setDescription(`${member} entrou no servidor, ${member.guild.name}, espero que se divirta!`)
 
         member.guild.channels.cache.get('906568789347418152').send({content: `${member}`, embeds: [welcomeEmbed]})
 
