1e8b4a9b096b6aff51a2e5e6a3ea9a4006c13813
Turtle <emanuelsadhot@gmail.com>
2022-07-18 13:15:20 -0300

    remove welcome message

--- src/events/guildMember/autorole.js
+++ src/events/guildMember/autorole.js
@@ -1,5 +1,4 @@
 const Event = require('../../structures/Event');
-const { MessageEmbed } = require('discord.js');
 
 module.exports = class extends Event {
     constructor(client) {
@@ -11,15 +10,5 @@ module.exports = class extends Event {
     run = async(member) => {
         const cargo = member.guild.roles.cache.get('906557014212235285')
         member.roles.add(cargo);
-        const avatarUrl = member.displayAvatarURL({ dynamic: true, size: 1024 })
-
-        const welcomeEmbed = new MessageEmbed()
-            .setTitle('Bem-vindo ao servidor!')
-            .setColor('GREEN')
-            .setImage(avatarUrl)
-            .setDescription(`${member} entrou no servidor, ${member.guild.name}, espero que se divirta!`)
-
-        member.guild.channels.cache.get('906568789347418152').send({content: `${member}`, embeds: [welcomeEmbed]})
-
     }
 }
