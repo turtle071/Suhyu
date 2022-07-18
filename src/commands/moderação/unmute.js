db3f945f61f8886c1813490c9264539f25ec5cda
Turtle <emanuelsadhot@gmail.com>
2022-07-18 13:16:26 -0300

    feat: add reason for unmute command

--- src/commands/moderação/unmute.js
+++ src/commands/moderação/unmute.js
@@ -5,13 +5,19 @@ module.exports = class extends Command {
     constructor(client) {
         super(client,{
             name: "unmute",
-            description: "Desmuta um usuário que esteja mutado.",
+            description: "Des-silencia um usuário que esteja mutado.",
             options: [
                 {
                     name: 'user',
                     type: 'USER',
-                    description: 'O usuário que será desmutado.',
+                    description: 'O usuário que será des-silenciar.',
                     required: true
+                },
+                {
+                    name: 'reason',
+                    type: 'STRING',
+                    description: 'A razão do usuário ser des-silenciado.',
+                    required: false
                 }
             ]
         });
@@ -20,11 +26,12 @@ module.exports = class extends Command {
     run = async (interaction) => {
         await interaction.deferReply({ ephemeral: false, fetchReply: true });
         if (!interaction.member.permissions.has('MODERATE_MEMBERS')) {
-            interaction.editReply('❌ | Você não pode desmutar usuários.');
+            interaction.editReply('❌ | Você não pode des-silenciar usuários.');
             return;
         }
         const user = interaction.options.getUser('user');
         const member = interaction.guild.members.cache.get(user.id);
+        const reason = interaction.options.getString('reason') ?? 'Nenhuma razão especificada.';
 
         if (!member) {
             interaction.editReply('❌ | Este usuário não está no servidor.');
@@ -32,17 +39,17 @@ module.exports = class extends Command {
         }
 
         if (interaction.user.id === member.id) {
-            interaction.editReply(':x: | Não é possível se desmutar!');
+            interaction.editReply(':x: | Não é possível usar este comando em si mesmo!');
             return;
         }
 
         if (member.roles) {
             if (!member.moderatable || member.roles.highest.position >= interaction.guild.me.roles.highest.position) {
-                interaction.editReply(':x: | Eu não posso desmutar este usuário.');
+                interaction.editReply(':x: | Eu não posso des-silenciar este usuário.');
                 return;
             }
             if (interaction.member.roles.highest.position <= member.roles.highest.position) {
-                interaction.editReply(':x: | Não foi possível desmutar este usuário, pois o cargo dele é maior que o meu.')
+                interaction.editReply(':x: | Não foi possível des-silenciar este usuário, pois o cargo dele é maior que o meu.')
                 return;
             }
         }
@@ -51,8 +58,8 @@ module.exports = class extends Command {
         member.timeout(unMute)
 
         const embed = new MessageEmbed()
-            .setTitle('Usuário desmutado!')
-            .setDescription(`:white_check_mark: | ${user.tag}.`)
+            .setTitle('Usuário des-silenciado!')
+            .setDescription(`:white_check_mark: | ${user.tag} | ${reason}.`)
             .setColor('#006400')
 
         interaction.editReply({ embeds: [embed] }).then(() => {
