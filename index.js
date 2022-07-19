14095c9cfac66940b7a0e3fc21016ba22d504e9a
Turtle <emanuelsadhot@gmail.com>
2022-07-19 15:03:06 -0300

    remove mention help in index

--- index.js
+++ index.js
@@ -10,15 +10,8 @@ const client = new Client({
         'GUILD_INVITES',
         'GUILD_VOICE_STATES',
         'GUILD_MEMBERS',
-        'GUILD_PRESENCES'        
+        'GUILD_PRESENCES'
     ]
 });
-
-//Mention help // @tortugo - reply this message
-client.on('messageCreate', async (message) => {
-    if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
-        message.reply(`Olá, ${message.author}, eu sou o ***Tortugo***, meu prefixo é \`/ \`. Se precisar de ajuda digite \`/help\`, e veja minha lista de comandos!`)
-    }
-})
 
 client.login(process.env.BOT_TOKEN);
