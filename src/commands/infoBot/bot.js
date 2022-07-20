c1c89907420db3f2fe5b5205865a4122d2c07318
Turtle <emanuelsadhot@gmail.com>
2022-07-19 21:32:54 -0300

    update link......

--- src/commands/infoBot/bot.js
+++ src/commands/infoBot/bot.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
         await interaction.deferReply({ephemeral: false, fetchReply: true})
         const avatar = this.client.user.displayAvatarURL({dynamic: false, size: 256})
         const embed = new MessageEmbed()
-            .setTitle(`Informações da suhyu`)
+            .setTitle(`Informações da Suhyu`)
             .setColor('RANDOM')
             .setThumbnail(avatar)
             .setDescription(`${this.client.user.username} está presente em ${this.client.guilds.cache.size} servidores.\n\nEntre no meu [servidor](https://discord.gg/jSgVkj3U9b)\n\nMe adicione [clicando aqui](https://bit.ly/3om8a7j)\n\n**Ping:** ${this.client.ws.ping}ms\n\n**Versão:** ${config.version}\n\n**Feito por:** ${config.author}`)
